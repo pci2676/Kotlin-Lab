dc10a62a4b3866c8c4e9dcc99b82377c0e7d7cf6
chanin <pci2676@gmail.com>
2021-02-21 23:50:50 +0900

    feat : chap 7 연산자 오버로딩 기본 example

---
+++ kotlin-in-action/src/main/kotlin/com/javabom/bomkotlin/part2/chap7/Point.kt
@@ -0,0 +1,7 @@
+package com.javabom.bomkotlin.part2.chap7
+
+data class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point): Point {
+        return Point(x + other.x, y + other.y)
+    }
+}
