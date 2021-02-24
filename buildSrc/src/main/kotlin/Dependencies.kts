3a9241d375885645590c386fbf13643f29bfe207
chanin <pci2676@gmail.com>
2021-02-25 07:34:19 +0900

    feat : 의존성 모듈 추가

---
+++ buildSrc/src/main/kotlin/Dependencies.kts
@@ -0,0 +1,7 @@
+object Versions {
+    const val kotlin = "1.4.21"
+    const val springDependencyManagement = "1.0.9.RELEASE"
+    const val springBoot = "2.3.8.RELEASE"
+    const val springCloud = "Hoxton.SR5"
+    const val kotlinLogging = "2.0.4"
+}
